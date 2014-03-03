60b697f0f5670172e8ab0bcf95d781c7124a6fe5
Josh Lauer <jlauer@cloudspace.com>
2014-03-03 12:07:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RssCloudspaceCom::Application.config.secret_key_base = '42da4cc39f1a6aaf7da18180c55781e5da8216d6b33da810bfd0e8d69714bba3d2a3c8f2a287c5f9da455cb24829cff4054620c9989d5e651b4347b9f2d87957'
