c9a0121bb22da6bcdde97ced49f6fbcff69b3516
Josh Lauer <jlauer@cloudspace.com>
2014-03-11 16:20:14 -0400

    modified .travis.yml to properly set secret_key_base and added comment to satisfy linter

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require 'rails/all'
 Bundler.require(:default, Rails.env)
 
 module CloudspaceRss
+  # the application itself
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
