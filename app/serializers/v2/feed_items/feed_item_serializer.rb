69a085c945df141d65789be983b87c58ff0d1b18
Josh Lauer <jlauer@cloudspace.com>
2014-03-18 18:50:33 -0400

    changed feed item serializer to put various image sizes at root

--- app/serializers/v2/feed_items/feed_item_serializer.rb
+++ app/serializers/v2/feed_items/feed_item_serializer.rb
@@ -1,15 +1,20 @@
 # Creates serialized output (JSON) for a FeedItem.
 class V2::FeedItems::FeedItemSerializer < ActiveModel::Serializer
-  attributes :id, :title, :url, :author, :images, :summary, :published_at, :created_at, :updated_at, :feed_id
+  attributes :id, :title, :url, :author, :summary, :published_at, :created_at, :updated_at,
+             :feed_id, :iphone_retina_image, :ipad_image, :ipad_retina_image
 
-  private
+  def iphone_retina_image
+    url = @object.image.url(:iphone_retina)
+    url.present? ? url : nil
+  end
 
-  def images
-    images = {}
-    object.image.styles.keys.each do |style|
-      url = object.image.url(style)
-      images[style] = url.blank? ? nil : url
-    end
-    images
+  def ipad_image
+    url = @object.image.url(:ipad)
+    url.present? ? url : nil
+  end
+
+  def ipad_retina_image
+    url = @object.image.url(:ipad_retina)
+    url.present? ? url : nil
   end
 end
