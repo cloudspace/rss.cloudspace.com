e5186d1019940fdc87efe2379f988fd59178c8f5
Joey Lorich <joey@cloudspace.com>
2014-05-05 12:39:41 -0400

    update production deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-role :app, %W{#{ENV['STAGING_APP_HOST']}}
-role :web, %W{#{ENV['STAGING_APP_HOST']}}
-role :db, %W{#{ENV['STAGING_APP_HOST']}}
+role :app, %W{#{ENV['PRODUCTION_APP_HOST']}}
+role :web, %W{#{ENV['PRODUCTION_APP_HOST']}}
+role :db, %W{#{ENV['PRODUCTION_APP_HOST']}}
 set :branch, 'production_deploy'
 set :rails_env, 'production'
 
