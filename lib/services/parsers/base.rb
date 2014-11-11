8120a76bc0e3d3a9ac41378f86f46eee8f23a04e
Alfredo Uribe <fellou89@cloudspace.com>
2014-11-11 10:08:29 -0500

    added exceptions to parser
    exceptions are then handled by rescue in feed model, and
      worker errors are generated to log the data
    refactored code to pass clint, and a test to look for exception

--- lib/services/parsers/base.rb
+++ lib/services/parsers/base.rb
@@ -3,7 +3,7 @@ class Service::Parser::Base
   include Service::Delegator
   include Service::Attributable
 
-  attr_accessor :options
+  attr_accessor :options, :code
 
   # override the default constructor to insert steps before #initialize is called
   def self.new(opts)
@@ -11,7 +11,25 @@ class Service::Parser::Base
     obj.options = Service::Options.new(obj.default_options).deep_merge!(opts)
     obj.send(:initialize)
     obj.parse
+    parse_exceptions(obj.code) if obj.code >= 400 || obj.code == 0
     obj
+  end
+
+  PARSER_EXCEPTIONS = {
+    0 => 'Not a URL',
+    400 => 'Bad Request',
+    401 => 'Unauthorized',
+    403 => 'Forbidden',
+    404 => 'Not Found',
+    408 => 'Timeout',
+    500 => 'Internal Server Error',
+    502 => 'Bad Gateway',
+    503 => 'Server Unavailable',
+    504 => 'Gateway Timeout'
+  }
+
+  def self.parse_exceptions(code)
+    fail PARSER_EXCEPTIONS[code]
   end
 
   def default_options
