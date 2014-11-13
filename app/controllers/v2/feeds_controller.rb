d0ae50df2d3c17de561df3af2b5d857c570a1e63
Alfredo Uribe <fellou89@cloudspace.com>
2014-11-13 11:04:59 -0500

    fixed FeedRequest creation
    it was placed after the return in Feed controller create action

--- app/controllers/v2/feeds_controller.rb
+++ app/controllers/v2/feeds_controller.rb
@@ -33,8 +33,8 @@ class V2::FeedsController < ApiController
   def create
     if params[:url].present?
       status = ensure_feed
-      return render(nothing: true, status: status) if status == :unprocessable_entity
       FeedRequest.find_or_create_by(feed_id: current_feed.id).count_update
+      return render(nothing: true, status: status) if status == :unprocessable_entity
     else
       return render nothing: true, status: :bad_request
     end
