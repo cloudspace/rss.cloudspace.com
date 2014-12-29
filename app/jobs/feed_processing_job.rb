0d5946182cede20780b2d705dd9545f1e35024bb
Michael Beattie <beattie@cloudspace.com>
2014-12-29 14:25:44 -0500

    [#84704374] Modified to pass clint

--- app/jobs/feed_processing_job.rb
+++ app/jobs/feed_processing_job.rb
@@ -18,16 +18,17 @@ class FeedProcessingJob < BaseResqueJob
     feed.feed_items.ready_for_processing.each do |feed_item|
       feed_item.update_attributes(scheduled: true)
       feed_item.lock_element!
-      url = "http://33.33.164.143:3000/api/flows/17/run?api_key=#{ENV['MICROSERVICE_API_KEY']}"\
-            "&url_48=#{feed_item.url}&s3_image_prefix_47=feed_items/#{feed_item.id}"\
-            "&new_image_name_47=iphone_retina&resize_string_47=640x800"\
-            "&s3_image_prefix_45=feed_items/#{feed_item.id}&new_image_name_45=ipad"\
-            "&resize_string_45=768x960&s3_image_prefix_46=feed_items/#{feed_item.id}"\
-            "&new_image_name_46=ipad_retina&resize_string_46=1526x1920"\
-            "&s3_prefix_44=feed_items/#{feed_item.id}&new_file_name_44=original"\
-            "&feed_item_id_49=#{feed_item.id}&feed_id_49=#{feed.id}&url_49=#{feed_item.url}"\
-            "&published_at_49=#{URI.encode(feed_item.published_at.to_s)}"\
-            "&title_49=#{URI.encode(feed_item.title)}&callback=http://33.33.164.176:3000/v2/callbacks/feed_item"
+      url = 'http://33.33.164.143:3000/api/flows/17/run?api_key=' + ENV['MICROSERVICE_API_KEY'] + ''\
+            '&url_48=' + feed_item.url.to_s + '&s3_image_prefix_47=feed_items/' + feed_item.id.to_s + ''\
+            '&new_image_name_47=iphone_retina&resize_string_47=640x800'\
+            '&s3_image_prefix_45=feed_items/' + feed_item.id.to_s + '&new_image_name_45=ipad'\
+            '&resize_string_45=768x960&s3_image_prefix_46=feed_items/' + feed_item.id.to_s + ''\
+            '&new_image_name_46=ipad_retina&resize_string_46=1526x1920'\
+            '&s3_prefix_44=feed_items/' + feed_item.id.to_s + '&new_file_name_44=original'\
+            '&feed_item_id_49=' + feed_item.id.to_s + '&feed_id_49=' + feed.id.to_s + ''\
+            '&url_49=' + feed_item.url.to_s + ''\
+            '&published_at_49=' + URI.encode(feed_item.published_at.to_s) + ''\
+            '&title_49=' + URI.encode(feed_item.title) + '&callback=http://33.33.164.176:3000/v2/callbacks/feed_item'
       HTTParty.get(url)
     end
   end
